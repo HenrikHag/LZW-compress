af2ec20dcd0344da9a5ba4ef607ed04c5d5c54c5
god245673 <god245673@DESKTOP-NHB0HIV.localdomain>
2023-10-08 23:27:33 +0200

    Started implementation of algorithm_lzw

---
+++ algorithm_lzw.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+void lzw_compress() {//std::ifstream input_file,std::ofstream output_file) {
+
+    // compression
+    std::map<char,int> lzw_dict;
+    char *result[5];
+
+    lzw_dict['a'] = 3;
+    std::cout << std::to_string(1);
+}
+
+int main(int argc, char const *argv[])
+{
+    lzw_compress();
+    return 0;
+}
